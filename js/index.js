2e8879c91aea4e407eca41fa46a3890364f92288
Tiktokshop-byte <wcemcoin@gmail.com>
2025-09-18 06:33:32 +0000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 
 function goTk() {
     var xhr = new XMLHttpRequest()
-    xhr.open("get", "/js/apkDownload.php", true);  //建立连接，要求异步响应
+    xhr.open("get", "/js/apkDownload.html", true);  //建立连接，要求异步响应
     xhr.send();  //发送请求 
     xhr.onreadystatechange = function () {  //绑定响应状态事件监听函数
        if (xhr.readyState == 4 && xhr.status == 200) {  //监听readyState状态和HTTP状态码
